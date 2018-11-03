bb0fcdf4c9155fd162a63e6a425781d11c9b98be
os1s <a1400239@myy.haaga-helia.fi>
2018-11-03 09:45:04 +0200

    fixed some stuff

--- livestriimertesti.py
+++ livestriimertesti.py
@@ -8,28 +8,24 @@ class Window(Frame):
         self.master = master
         self.init_window()
 
-    def alkaizerx(self, chat=False):
-        if chat == False:
-            os.system('C:\\Users\\scrsm\\AppData\\Local\\Programs\\Python\\Python37-32\\Scripts\\livestreamer.exe twitch.tv/alkaizerx best')
-        else:
-            webbrowser.open('https://www.twitch.tv/popout/alkaizerx/chat')
-
+    def alkaizerx(self):
+        os.system('C:\\Users\\scrsm\\AppData\\Local\\Programs\\Python\\Python37-32\\Scripts\\livestreamer.exe twitch.tv/alkaizerx best')
 
     def b0aty(self):
         os.system('C:\\Users\\scrsm\\AppData\\Local\\Programs\\Python\\Python37-32\\Scripts\\livestreamer.exe twitch.tv/b0aty best')
 
-    #def alkchat(self):
-    #    webbrowser.open('https://www.twitch.tv/popout/alkaizerx/chat')
+    def alkchat(self):
+        webbrowser.open('https://www.twitch.tv/popout/alkaizerx/chat')
 
     def init_window(self):
 
-        self.master.title('paska')
+        self.master.title(':^)')
         self.pack(fill=BOTH, expand=1)
 
         butt = Button(self, text='alkaizerX', width=12, height=2, command=self.alkaizerx)
         butt.place(x=150, y=0)
 
-        butt = Button(self, text='chat', width=12, height=2, command=self.alkaizerx(chat=True))
+        butt = Button(self, text='chat', width=12, height=2, command=self.alkchat)
         butt.place(x=250, y=0)
 
         butt = Button(self, text='b0aty', width=12, height=2, command=self.b0aty)
@@ -39,7 +35,3 @@ top = Tk()
 top.geometry("400x300")
 app = Window(top)
 top.mainloop()
-
-
-#def streamer(self, name):
-#    os.system('C:\\Users\\scrsm\\AppData\\Local\\Programs\\Python\\Python37-32\\Scripts\\livestreamer.exe twitch.tv/'+name+' best')
