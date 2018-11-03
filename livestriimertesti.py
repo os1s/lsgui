869d310248e459256687920e7a955d24557e743b
os1s <a1400239@myy.haaga-helia.fi>
2018-11-03 14:06:01 +0200

    more buttons

--- livestriimertesti.py
+++ livestriimertesti.py
@@ -14,11 +14,17 @@ class Window(Frame):
     def b0aty(self):
         os.system('C:\\Users\\scrsm\\AppData\\Local\\Programs\\Python\\Python37-32\\Scripts\\livestreamer.exe twitch.tv/b0aty best')
 
+    def joniosan(self):
+        os.system('C:\\Users\\scrsm\\AppData\\Local\\Programs\\Python\\Python37-32\\Scripts\\livestreamer.exe twitch.tv/joniosan best')
+
     def alkchat(self):
         webbrowser.open('https://www.twitch.tv/popout/alkaizerx/chat')
 
     def b0atychat(self):
         webbrowser.open('https://www.twitch.tv/popout/b0aty/chat')
+
+    def jonichat(self):
+        webbrowser.open('https://www.twitch.tv/popout/joniosan/chat')
 
     def init_window(self):
 
@@ -26,16 +32,24 @@ class Window(Frame):
         self.pack(fill=BOTH, expand=1)
 
         butt = Button(self, text='alkaizerX', width=12, height=2, command=self.alkaizerx)
-        butt.place(x=150, y=0)
+        butt.place(x=100, y=0)
 
         butt = Button(self, text='chat', width=12, height=2, command=self.alkchat)
-        butt.place(x=250, y=0)
+        butt.place(x=200, y=0)
 
         butt = Button(self, text='b0aty', width=12, height=2, command=self.b0aty)
-        butt.place(x=150, y=50)
+        butt.place(x=100, y=50)
 
         butt = Button(self, text='chat', width=12, height=2, command=self.b0atychat)
-        butt.place(x=250, y=0)
+        butt.place(x=200, y=50)
+
+        butt = Button(self, text='joniosan', width=12, height=2, command=self.joniosan)
+        butt.place(x=100, y=100)
+
+        butt = Button(self, text='chat', width=12, height=2, command=self.jonichat)
+        butt.place(x=200, y=100)
+
+
 
 top = Tk()
 top.geometry("400x300")
