a105258429ce164b1d4c5555523af2e9702db7b6
os1s <a1400239@myy.haaga-helia.fi>
2018-11-07 08:20:22 +0200

    made functions better

--- livestriimertesti.py
+++ livestriimertesti.py
@@ -9,54 +9,36 @@ class Window(Frame):
         self.master = master
         self.init_window()
 
-    def handle_click1(self):
-        def alkaizerx():
-            os.system('C:\\Users\\scrsm\\AppData\\Local\\Programs\\Python\\Python37-32\\Scripts\\livestreamer.exe twitch.tv/alkaizerx best')
-        t = threading.Thread(target=alkaizerx)
+    def handle_stream(self, streamer):
+        def openstream():
+            os.system('C:\\Users\\scrsm\\AppData\\Local\\Programs\\Python\\Python37-32\\Scripts\\livestreamer.exe twitch.tv/'+streamer+' best')
+        t = threading.Thread(target=openstream)
         t.start()
 
-    def handle_click2(self):
-        def b0aty():
-            os.system('C:\\Users\\scrsm\\AppData\\Local\\Programs\\Python\\Python37-32\\Scripts\\livestreamer.exe twitch.tv/b0aty best')
-        t = threading.Thread(target=b0aty)
-        t.start()
-
-    def handle_click3(self):
-        def joniosan():
-            os.system('C:\\Users\\scrsm\\AppData\\Local\\Programs\\Python\\Python37-32\\Scripts\\livestreamer.exe twitch.tv/joniosan best')
-        t = threading.Thread(target=joniosan)
-        t.start()
-
-    def alkchat(self):
-        webbrowser.open('https://www.twitch.tv/popout/alkaizerx/chat')
-
-    def b0atychat(self):
-        webbrowser.open('https://www.twitch.tv/popout/b0aty/chat')
-
-    def jonichat(self):
-        webbrowser.open('https://www.twitch.tv/popout/joniosan/chat')
+    def open_chat(self, streamer):
+        webbrowser.open('https://www.twitch.tv/popout/'+streamer+'/chat')
 
     def init_window(self):
 
         self.master.title(':^)')
         self.pack(fill=BOTH, expand=1)
 
-        butt = Button(self, text='alkaizerX', width=12, height=2, command=self.handle_click1)
+        butt = Button(self, text='alkaizerX', width=12, height=2, command=lambda: self.handle_stream('alkaizerx'))
         butt.place(x=100, y=0)
 
-        butt = Button(self, text='chat', width=12, height=2, command=self.alkchat)
+        butt = Button(self, text='chat', width=12, height=2, command=lambda: self.open_chat('alkaizerx'))
         butt.place(x=200, y=0)
 
-        butt = Button(self, text='b0aty', width=12, height=2, command=self.handle_click2)
+        butt = Button(self, text='b0aty', width=12, height=2, command=lambda: self.handle_stream('b0aty'))
         butt.place(x=100, y=50)
 
-        butt = Button(self, text='chat', width=12, height=2, command=self.b0atychat)
+        butt = Button(self, text='chat', width=12, height=2, command=lambda: self.open_chat('b0aty'))
         butt.place(x=200, y=50)
 
-        butt = Button(self, text='joniosan', width=12, height=2, command=self.handle_click3)
+        butt = Button(self, text='joniosan', width=12, height=2, command=lambda: self.handle_stream('joniosan'))
         butt.place(x=100, y=100)
 
-        butt = Button(self, text='chat', width=12, height=2, command=self.jonichat)
+        butt = Button(self, text='chat', width=12, height=2, command=lambda: self.open_chat('joniosan'))
         butt.place(x=200, y=100)
 
 top = Tk()
